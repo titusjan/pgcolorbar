668e31cf4a8a2f8659fc363ead196b4f45082a66
Pepijn Kenter <titusjan@gmail.com>
2017-04-22 18:34:11 +0200

    Using separate ImageItem and HistogramLUTItem.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import sys
 import numpy as np
 from pyqtgraph.Qt import QtCore, QtGui
+
 import pyqtgraph as pg
 
 def main():
@@ -13,43 +14,39 @@ def main():
     ## Create window with ImageView widget
     win = QtGui.QMainWindow()
     win.resize(800,800)
-    imv = pg.ImageView()
-    win.setCentralWidget(imv)
+
+
+    imagePlotItem = pg.PlotItem()
+
+    viewBox = imagePlotItem.getViewBox()
+    viewBox.disableAutoRange(pg.ViewBox.XYAxes)
+
+    imageItem = pg.ImageItem()
+    imagePlotItem.addItem(imageItem)    
+
+    ## Create random 3D data set with noisy signals
+    img = pg.gaussianFilter(np.random.normal(size=(300, 200)), (5, 5)) * 20 + 100
+    nRows, nCols = img.shape
+    imageItem.setImage(img)
+    imagePlotItem.setRange(xRange=[0, nCols], yRange=[0, nRows])
+    
+    
+    histLutItem = pg.HistogramLUTItem() # what about GradientLegend?
+    histLutItem.setImageItem(imageItem)
+    histLutItem.vb.setMenuEnabled(False)
+    histLutItem.setHistogramRange(90, 110) # Disables autoscaling
+
+    graphicsLayoutWidget = pg.GraphicsLayoutWidget()
+
+    graphicsLayoutWidget.addItem(imagePlotItem, 0, 0)
+    graphicsLayoutWidget.addItem(histLutItem, 0, 1)
+
+
+    # imv = pg.ImageView()
+    win.setCentralWidget(graphicsLayoutWidget)
     win.show()
     win.setWindowTitle('pyqtgraph example: ImageView')
 
-    ## Create random 3D data set with noisy signals
-    img = pg.gaussianFilter(np.random.normal(size=(200, 200)), (5, 5)) * 20 + 100
-    img = img[np.newaxis,:,:]
-    decay = np.exp(-np.linspace(0,0.3,100))[:,np.newaxis,np.newaxis]
-    data = np.random.normal(size=(100, 200, 200))
-    data += img * decay
-    data += 2
-
-    ## Add time-varying signal
-    sig = np.zeros(data.shape[0])
-    sig[30:] += np.exp(-np.linspace(1,10, 70))
-    sig[40:] += np.exp(-np.linspace(1,10, 60))
-    sig[70:] += np.exp(-np.linspace(1,10, 30))
-
-    sig = sig[:,np.newaxis,np.newaxis] * 3
-    data[:,50:60,30:40] += sig
-
-
-    ## Display the data and assign each frame a time value from 1.0 to 3.0
-    imv.setImage(data, xvals=np.linspace(1., 3., data.shape[0]))
-
-    ## Set a custom color map
-    colors = [
-        (0, 0, 0),
-        (45, 5, 61),
-        (84, 42, 55),
-        (150, 87, 60),
-        (208, 171, 141),
-        (255, 255, 255)
-    ]
-    cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 6), color=colors)
-    imv.setColorMap(cmap)
 
     QtGui.QApplication.instance().exec_()
 
